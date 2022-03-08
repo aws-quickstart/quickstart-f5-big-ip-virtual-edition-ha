02768e9ee9104bb045160f0cb09beeaed51fb687
Michael Shimkus <m.shimkus@f5.com>
2022-03-08 14:18:07 -0800

    updates to verify application test

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/common/verify_application_02.sh
+++ automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/common/verify_application_02.sh
@@ -16,10 +16,10 @@ else
     bigip2_stack_name=$(aws cloudformation describe-stacks --stack-name $workload_stack_name --region <REGION> | jq -r '.Stacks[].Outputs[] | select (.OutputKey=="bigipInstance02") | .OutputValue')
     echo "bigip2_stack_name: $bigip2_stack_name"
 
-    bigip1_instanceid=$(aws cloudformation describe-stacks --stack-name $bigip2_stack_name --region <REGION> | jq -r '.Stacks[].Outputs[] | select (.OutputKey=="bigIpInstanceId") | .OutputValue')
-    echo "bigip1_instanceid: $bigip1_instanceid"
+    bigip2_instanceid=$(aws cloudformation describe-stacks --stack-name $bigip2_stack_name --region <REGION> | jq -r '.Stacks[].Outputs[] | select (.OutputKey=="bigIpInstanceId") | .OutputValue')
+    echo "bigip2_instanceid: $bigip2_instanceid"
 
-    application_public_ip=$(aws ec2 describe-instances --region <REGION> --instance-ids $bigip1_instanceid | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[] |select (.Primary=='false') | .Association.PublicIp')
+    application_public_ip=$(aws ec2 describe-instances --region <REGION> --instance-ids $bigip2_instanceid | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[] |select (.Primary=='false') | .Association.PublicIp')
     echo "application_public_ip: $application_public_ip"
 
     httpsResponse=$(curl -sk https://$application_public_ip)
