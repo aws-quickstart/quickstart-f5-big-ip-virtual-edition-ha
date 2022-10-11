2881aaa530272d016e7c04fdb2f080d19c6d9075
Michael Shimkus <m.shimkus@f5.com>
2022-10-11 14:24:45 -0700

    removed lint disable from tests

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/create_stack.sh
+++ automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/create_stack.sh
@@ -3,4 +3,4 @@
 #  replayEnabled = false
 #  replayTimeout = 0
 
-taskcat test run --lint-disable -n -o taskcat_outputs/ -i automated-test-scripts/data/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/<CONFIG FILE>
+taskcat test run -n -o taskcat_outputs/ -i automated-test-scripts/data/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/<CONFIG FILE>
