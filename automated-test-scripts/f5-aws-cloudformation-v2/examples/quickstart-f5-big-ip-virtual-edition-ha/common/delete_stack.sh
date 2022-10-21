c8e280c0497f1c603a62c47ada41e0e714e9af78
shimkus <m.shimkus@f5.com>
2022-10-21 22:20:39 +0000

    Updating develop...

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/common/delete_stack.sh
+++
@@ -1,8 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-stack_name=$(cat taskcat_outputs/tC* | grep  -m1 StackName: | cut -d":" -f2)
-echo $stack_name
-aws cloudformation delete-stack --region <REGION> --stack-name $stack_name
-echo "PASS"
--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/create_stack.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "CREATE_COMPLETE"
-#  scriptTimeout = 30
-#  replayEnabled = false
-#  replayTimeout = 0
-
-taskcat test run -n -o taskcat_outputs/ -i automated-test-scripts/data/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/<CONFIG FILE>
