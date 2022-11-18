6145504d517c811f0dcb5c8ee925c06ef81802fa
shimkus <m.shimkus@f5.com>
2022-11-18 20:34:21 +0000

    Updating develop...

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart-f5-big-ip-virtual-edition-ha/common/delete_secret.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "ARN"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws secretsmanager delete-secret --secret-id <DEWPOINT JOB ID>-secret --region <REGION> | jq -r .
